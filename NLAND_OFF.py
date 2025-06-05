4f51920c67c293a271f8328fd04ecb58a091b28a
DTS-testdev <jinz3temp@gmail.com>
2025-06-06 03:53:05 +0900

    Update NLAND_OFF.py

--- NLAND_OFF.py
+++ NLAND_OFF.py
@@ -5,6 +5,7 @@ import re
 import time
 import random
 import os
+import ast
 
 # ================================================
 
@@ -12,6 +13,8 @@ api_token = os.getenv("API_TOKEN")
 webhook_url_new = os.getenv("WEBHOOK_URL_NEW")
 webhook_url_json = os.getenv("WEBHOOK_URL_JSON")
 webhook_url_ping = os.getenv("WEBHOOK_URL_PING")
+rCID = os.getenv("RCID")
+sURL = os.getenv("SURL")
 
 # ================================================
 
@@ -37,13 +40,44 @@ def string_to_number (string):
     return number_output
 
 
-def send_slack_message(message, webhook_url):
+def send_msg(message, webhook_url):
     payload = {'text': message}
     headers={
         'Content-Type' : 'application/json'
     }
     resp = requests.post(webhook_url, headers=headers, data=json.dumps(payload))
     return resp
+
+
+def recall_last_msg(
+        api_token = api_token,
+        channel_id = rCID,
+        no_msg_return = []
+):
+
+    headers = {
+        'Authorization': f'Bearer {api_token}',
+    }
+    params = {
+        'channel': channel_id,
+        'limit': 1,
+    }
+
+    response = requests.get(sURL, headers=headers, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        if data['ok']:
+            messages = data['messages']
+            if messages:
+                last_message = messages[0]['text']
+                return last_message
+            else:
+                return no_msg_return
+        else:
+            return no_msg_return
+    else:
+        return no_msg_return
 
 # ================================================
 
@@ -65,10 +99,10 @@ for parameter in parameters:
 
         if response.status_code != 200:
             print('invalid status: %d' % response.status_code)
-            send_slack_message(f"[NLAND] {parameter['hscpNo']}-{page}\n{'invalid status: %d' % response.status_code}", webhook_url=webhook_url_ping)
+            send_msg(f"[NLAND] {parameter['hscpNo']}-{page}\n{'invalid status: %d' % response.status_code}", webhook_url=webhook_url_ping)
             break
         else:
-            send_slack_message(f"[NLAND] {parameter['hscpNo']}-{page}\n{'valid status: %d' % response.status_code}", webhook_url=webhook_url_ping)
+            send_msg(f"[NLAND] {parameter['hscpNo']}-{page}\n{'valid status: %d' % response.status_code}", webhook_url=webhook_url_ping)
 
         data = json.loads(response.text)
         result = data['result']
@@ -112,8 +146,7 @@ json_filename = 'NLAND_OFF.json'
 json_directory = os.path.join(os.getcwd(),'pyprojects', json_filename)
 
 try:
-    with open (json_directory, 'r') as f:
-        list_saved = json.load(f)
+    list_saved = ast.literal_eval(recall_last_message())
 except:
     list_saved = []
 
@@ -124,8 +157,6 @@ if not filtered_df.empty:
     for idx, row in filtered_df.iterrows():
         enlist_ok = '🔥🔥🔥🔥🔥🔥🔥\n' if row['enlist'] else ''
         message_short = f"{enlist_ok}[{row['deposit']}/{row['rate']}] {row['building']} {row['floor']}층\n계약면적 {row['area_trans']}평, 전용면적 {row['area_real']}평\n{row['comment']}\n{row['agent']} {row['updated']}\n{url_prefix}{row['articleno']}"
-        send_slack_message(message_short, webhook_url=webhook_url_new)
-        with open (json_directory, 'w') as f:
-            list_all = list_saved + list_new
-            json.dump(list_all, f)
-    send_slack_message(f"Item No. {str(list_new)} is added to NLAND_OFF.json file.", webhook_url=webhook_url_json)
+        send_msg(message_short, webhook_url=webhook_url_new)
+    list_all = list_saved + list_new
+    send_msg(str(list_all), webhook_url=webhook_url_json)
